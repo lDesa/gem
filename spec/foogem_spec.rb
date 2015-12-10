3d4125b30df42cd5c4dbd500a9812c6776ef1afb
Vyacheslav Romanov <v.roomanov@gmail.com>
2015-12-10 21:49:28 +0300

    test addition is green

--- spec/foogem_spec.rb
+++ spec/foogem_spec.rb
@@ -1,4 +1,5 @@
-require 'spec_helper'
+require  'spec_helper'
+
 
 describe Foogem do
   it 'has a version number' do
@@ -9,9 +10,10 @@ describe Foogem do
   #   expect(false).to eq(true)
   # end
 
+  let(:dummy_class) { (Class.new { include Foogem }).new}
   
   it "addition 100 to number " do
-  	addition_100(10).should == 110
+  dummy_class.addition_100(10).should == 110
 
 	end
  
